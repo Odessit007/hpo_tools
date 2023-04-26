3e1f6242522f980838041112688b30d653b00421
Odessit007 <igor.vustyanyuk@gmail.com>
2023-04-26 03:43:16 +0300

    Update README. Add annotation and ontology parsing support

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# hpo_tools
+A set of open-source tools to simplify phenotypic research.
+
+The epic plan:
+* basic ontology interaction utils
+  * [Phase 1] download and read .obo file (via Pronto)
+  * [Phase 1] get node ancestors for arbitrary distance
+  * [Phase 1] get node depth
+  * [Phase 2] get distance between two nodes
+  * [Phase 2] sort nodes in lexicographic order
+* similarity
+  * [Phase 1] information content-based similarity:
+    * basic annotation utils
+    * Resnik
+    * Lin and variations
+    * JC
+    * intersection over union
+    * set-to-set similarity
+  * [Phase 2] node embeddings based on their text definitions
+  * [Phase 2] Word2Vec-based node embeddings
+  * [Phase 2] graph neural networks-based node embeddings
+* sampling
+  * [Phase 1] random terms
+  * [Phase 1] random sets of terms
+  * [Phase 2] random paths starting from the given term with given state change probabilities
+* p-values
+  * [Phase 1] set-to-set similarity for random/fixed content and size of the term sets
+  * [Phase 1] simple multiple testing correction
+* [Phase 2] check HPO terms
+  * if they are deprecated suggest new options
+  * if they are missing suggest updating HPO version
+  * generate report
+* [Phase 2] visualization
+  * sub-graphs
+  * similarity
+    * heat matrix
+    * "vertical" plot
+  * phenograms for cohort analysis
+    * sub-graph with nodes alpha proportional to some statistics within the cohort
+    * cohort VS full dataset
+
+
+The goal of phase 1 is to be able to reproduce the Phenomizer paper.
---
+++ hpo_tools/annotation.py
@@ -0,0 +1,104 @@
+from collections import defaultdict
+import datetime
+import json
+import hashlib
+import logging
+import os
+import pickle
+from typing import (
+    Dict,
+    List,
+    Literal
+)
+
+import numpy as np
+
+
+from hpo_tools.ontology import Ontology
+
+
+DataDict = Dict[str, List[str]]
+
+
+class Annotation:
+    def __init__(
+            self,
+            entity2hpo: DataDict,
+            ontology: Ontology,
+            max_expansion: int = -1,
+            name: str = '',
+            on_missing_terms: Literal['accept', 'skip', 'raise'] = 'accept'
+    ):
+        self.name = name
+        self.input_hash = hashlib.md5(json.dumps(entity2hpo, sort_keys=True).encode('utf8')).hexdigest()
+        self.entity2hpo = {key: set(vals) for key, vals in entity2hpo.items()}
+        self.ontology = ontology
+        self.max_expansion = max_expansion
+        self.on_missing_terms = on_missing_terms
+        self.indexer = {entity: i for i, entity in enumerate(entity2hpo)}
+        self.inverse_indexer = np.array([entity for i, entity in enumerate(entity2hpo)])
+        self.hpo2entity, self.n_input_entities, self.N = self._annotate()
+        self.counter = self._get_counts()
+        self.ic = self._get_ic()
+
+    def _get_missing_terms(self, hpo_ids):
+        return [hpo_id for hpo_id in hpo_ids if hpo_id not in self.ontology.pheno_indexer]
+
+    def _get_missing_terms_report(self) -> DataDict:
+        timestamp = datetime.datetime.now().strftime('%d-%m-%Y-%h-%m-%s')
+        # TODO Add alternative terms to the report:
+        #   alternate_ids: map them to the main ID
+        #   replaced_by for obsolete terms: map main ID to them
+        #   consider for obsolete terms: map main ID to them
+
+        # TODO Extract the log path somewhere
+        log_path = os.path.join(
+            os.environ['HOME'],
+            'hpo_tools',
+            f'missing_terms_report{"_" + self.name if self.name else ""}_{timestamp}.log'
+        )
+        report = {key: self._get_missing_terms(hpo_ids) for key, hpo_ids in self.entity2hpo.items()}
+        report = {key: hpo_ids for key, hpo_ids in report.items() if hpo_ids}
+        if not report:
+            logging.info('All terms are present in ontology.')
+            return {}
+        with open(log_path, 'w') as fout:
+            json.dump(report, fout)
+            logging.warning(f'Unknown HPO terms encountered. Check entities with missing terms in {log_path} file.')
+        return report
+
+    def _annotate(self):
+        report = self._get_missing_terms_report()
+        if report and self.on_missing_terms == 'raise':
+            raise RuntimeError(f'Missing HPO terms found. Check report.')
+        if self.on_missing_terms == 'skip':
+            proper_entities = [key for key in self.entity2hpo if key not in report]
+        else:
+            proper_entities = [key for key, hpo_ids in self.entity2hpo.items() if len(report.get(key, [])) < len(hpo_ids)]
+        n_proper_entities = len(proper_entities)
+        n_input_entities = len(self.entity2hpo)
+        results = defaultdict(set)
+        for entity, hpo_ids in self.entity2hpo.items():
+            nodes = {self.ontology.pheno_indexer[hpo_id] for hpo_id in hpo_ids if hpo_id in self.ontology.pheno_indexer}
+            if not nodes:
+                continue
+            if self.max_expansion:
+                nodes = self.ontology.get_nodeset_ancestors(nodes, self.max_expansion)
+            for node in nodes:
+                results[node].add(self.indexer[entity])
+        return results, n_input_entities, n_proper_entities
+    
+    def _get_counts(self):
+        return np.array([len(self.hpo2entity[node]) for node in self.ontology.nodes])
+
+    def _get_ic(self):
+        return self.counter / self.N
+
+    def to_pickle(self, file):
+        with open(file, 'wb') as fout:
+            pickle.dump(self, fout)
+
+    @staticmethod
+    def from_pickle(file):
+        with open(file, 'rb') as fin:
+            return pickle.load(fin)

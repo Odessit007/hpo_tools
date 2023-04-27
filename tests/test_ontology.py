a5b09a437759aabadd02ee8afd08d30875b33168
Odessit007 <igor.vustyanyuk@gmail.com>
2023-04-27 03:31:35 +0300

    Fix test

--- tests/test_ontology.py
+++ tests/test_ontology.py
@@ -1,7 +1,13 @@
+from typing import List
+
 from hpo_tools.ontology import Ontology
 
 
 class TestBuild:
+    @staticmethod
+    def _get_term_set(ontology: Ontology, nodes: List[int]):
+        return {ontology.inverse_pheno_indexer[node] for node in nodes}
+
     def test_from_obo_file(self):
         path = "data/hp.obo"
         ontology = Ontology.from_obo_file(path)
@@ -9,7 +15,7 @@ class TestBuild:
         term = "HP:0041117"
         node = ontology.pheno_indexer[term]
         assert ontology.names[node] == "Fractured lower limb segment", "Name mismatch"
-        parents = {ontology.inverse_pheno_indexer(p) for p in ontology.parents[node]}
+        parents = self._get_term_set(ontology, ontology.parents[node])
         assert parents == {"HP:0040064"}, "Parent mismatch"
-        children = [ontology.inverse_pheno_indexer(c) for c in ontology.children[node]]
+        children = self._get_term_set(ontology, ontology.children[node])
         assert children == {"HP:0041064", "HP:0041162"}
